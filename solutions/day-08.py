d2779e242a60fbf81e981daafd7046b4180b059e
Rose Nasrawi <r.nasrawi11@gmail.com>
2024-12-08 17:31:52 +0100

    solve day 8

--- solutions/day-08.py
+++ solutions/day-08.py
@@ -3,40 +3,58 @@ from itertools import combinations as comb
 
 # --- Day 8: Resonant Collinearity ---
 
-data = getinput('08', example=False)
-data = [list(map(str, line)) for line in data]
+data = getinput('08', example=True)
 
 def find_antennae(data):
+
     antennae = {}
+
     for y, line in enumerate(data):
         for x, char in enumerate(line):
             if char != '.':
                 antennae.setdefault(char, []).append((x, y))
+                
     return antennae
-
-antennae = find_antennae(data)
 
 def inbound(data, x, y):
     return 0 <= x < len(data[0]) and 0 <= y < len(data)
 
-def gen_antinodes(data, antennae):
-    freq = list(antennae.keys())
-    antinodes = 0
+def gen_antinodes(data, harmonics=False):
 
-    for f in freq:
-        for combo in comb(antennae[f], 2):
+    antennae = find_antennae(data)
+    datmap = [list(map(str, line)) for line in data]
+
+    for positions in antennae.values():
+        for combo in comb(positions, 2):
             (ax, ay), (bx, by) = combo
             dx, dy = ax - bx, ay - by
-        
-            cx, cy = ax + dx, ay + dy
-            dx, dy = bx - dx, by - dy
 
-            for x, y in [(cx, cy), (dx, dy)]:
-                if inbound(data, x, y) and data[y][x] != '#':
-                    data[y][x] = '#'
-                    antinodes += 1
+            ax += dx; ay += dy
+            bx -= dx; by -= dy
 
-    return antinodes
+            for x, y in [(ax, ay), (bx, by)]:
+                if inbound(datmap, x, y) and datmap[y][x] != '#':
+                    datmap[y][x] = '#'
 
-antinodes = gen_antinodes(data, antennae)
-print('Part 1,', antinodes)
+            if harmonics:
+
+                anti = set()
+                while inbound(datmap, ax+dx, ay+dy) or inbound(datmap, bx-dx, by-dy):
+                    ax += dx; ay += dy
+                    bx -= dx; by -= dy
+                    if inbound(datmap, ax, ay):
+                        anti.add((ax,ay))
+                    if inbound(datmap, bx, by):
+                        anti.add((bx,by))
+                    
+                for x,y in anti:
+                    if datmap[y][x] != '#':
+                        datmap[y][x] = '#'
+
+    if harmonics: 
+        return sum(char != '.' for line in datmap for char in line)
+    else:
+        return sum(char == '#' for line in datmap for char in line)
+
+print('Part 1:', gen_antinodes(data))
+print('Part 2:', gen_antinodes(data, harmonics=True))
