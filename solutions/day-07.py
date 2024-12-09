a700f69209acce2df37a37ff22d42563c349bcd0
Rose Nasrawi <r.nasrawi11@gmail.com>
2024-12-09 12:11:56 +0100

    solve day 7

--- solutions/day-07.py
+++ solutions/day-07.py
@@ -3,24 +3,37 @@ import regex as re
 
 # --- Day 7: Bridge Repair ---
 
-data = getinput('07', example=True)
+data = getinput('07', example=False)
 
-def check_valid(result, values):
+def check_valid(result, values, canconcat = False):
 
     val, rest = values[0], values[1:]
 
     if len(rest) == 1:
-        if result in [val+rest[0], val*rest[0]]:
+        if result == val+rest[0]:
             return True
+        elif result == val*rest[0]:
+            return True
+        
+        if canconcat:
+            if result == int(str(val)+str(rest[0])):
+                return True
     
     elif len(rest) > 1:
         plus = [val+rest[0]]+rest[1:]
         times = [val*rest[0]]+rest[1:]
 
-        if check_valid(result, plus) or check_valid(result, times):
-            return True
+        concat = int(str(val)+str(rest[0]))
+        concat = [concat]+rest[1:]
 
-total = 0
+        if canconcat:
+            if check_valid(result, plus, canconcat) or check_valid(result, times, canconcat) or check_valid(result, concat, canconcat):
+                return True
+        else:
+            if check_valid(result, plus) or check_valid(result, times):
+                return True
+
+total_1, total_2 = 0,0
 
 for line in data:
     result, values = line.split(':')
@@ -28,6 +41,10 @@ for line in data:
     values = list(map(int, re.findall(r'\d+', values)))
 
     if check_valid(result, values):
-        total += result
+        total_1 += result
+    
+    if check_valid(result, values, True):
+        total_2 += result
 
-print('Part 1:', total)
+print('Part 1:', total_1)
+print('Part 2:', total_2)
