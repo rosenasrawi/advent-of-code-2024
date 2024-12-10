6646b7bd42e6770c4a709d93c321f5681bef25cb
Rose Nasrawi <r.nasrawi11@gmail.com>
2024-12-10 15:35:35 +0100

    solve day 10 pt 1

---
+++ solutions/day-10.py
@@ -0,0 +1,53 @@
+from _getinput import *
+
+# --- Day 10: Hoof It ---
+
+data = getinput('10',example=False)
+
+trailhead = []
+
+for x,line in enumerate(data):
+    for y,char in enumerate(line):
+        if char == '0':
+            trailhead.append([x,y])
+
+directions = ((-1,0), (1,0), (0,-1), (0,1))
+
+def out_of_bounds(x,y):
+    return not (0 <= x < len(data) and 0 <= y < len(data[0]))
+
+def trail_search(trail):
+    queue = [trail]
+    visited = []
+    score = 0
+
+    while queue:
+        x,y = queue.pop(0)
+        visited.append((x,y))
+        prev = int(data[x][y])
+
+        for dx,dy in directions:
+            nx, ny = x+dx, y+dy
+
+            if out_of_bounds(nx,ny) or (nx,ny) in visited:
+                continue
+
+            next = int(data[nx][ny])
+
+            if next-prev == 1:
+
+                if next == 9:
+                    visited.append((nx,ny))
+                    score+=1
+                    continue
+
+                queue.append((nx,ny))
+
+    return score
+
+total = 0
+
+for trail in trailhead:
+    total += trail_search(trail)
+
+print('Part 1:', total)
