280edf4a16f775efc54b2d8bd998bfc241b80913
Rose Nasrawi <r.nasrawi11@gmail.com>
2024-12-22 22:11:23 +0100

    solve day 22

--- solutions/day-22.py
+++ solutions/day-22.py
@@ -2,7 +2,7 @@ from _getinput import *
 
 # --- Day 22: Monkey Market ---
 
-data = getinput('22', example=True)
+data = getinput('22', example=False)
 numbers = list(map(int, data))
 
 def mix(new_secret, secret):
@@ -29,10 +29,38 @@ def sequence(secret):
 
     return new_secret
 
+seqs = []
 total = 0
 for secret in numbers:
+    new_seq = []
     for _ in range(2000):
         secret = sequence(secret)
+        new_seq.append(int(str(secret)[-1]))
     total += secret
+    seqs.append(new_seq)
 
-print(total)
+for i, n in enumerate(numbers):
+    seqs[i].insert(0, n)
+
+all_scores = {}
+
+for seq in seqs:
+
+    scores = {}
+    
+    for slide in range(len(seqs[0])-5):
+        nums = seq[slide:slide+5]
+        difs = tuple([nums[i+1]-nums[i] for i in range(4)])
+        score = seq[slide+4]
+
+        if difs not in scores:
+            scores[difs] = score
+
+    for key in scores:
+        if key in all_scores:
+            all_scores[key] += scores[key]
+        else:
+            all_scores[key] = scores[key]
+
+print('Part 1:', total)
+print('Part 2:', max(all_scores.values()))
